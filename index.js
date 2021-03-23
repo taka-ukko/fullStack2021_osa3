6a9070c26fe6d591d35a8a89d42571d44e1bd6c3
taka-ukko <taka.ukko@gmail.com>
2021-03-23 08:22:33 +0200

    Create heroku app

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -83,7 +86,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})  
+})   
